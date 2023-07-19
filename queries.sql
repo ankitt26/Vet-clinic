61375eb223fd83fd732efc009e07fad214d0f16f
Ankit <ankit26kushwa@gmail.comm>
2023-07-19 16:21:12 +0530

    Add transactions in queries.sql

--- queries.sql
+++ queries.sql
@@ -15,3 +15,31 @@ SELECT * FROM animals WHERE neutered = true ;
 SELECT * FROM animals WHERE name <> 'Gabumon' ;
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3 ;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified' ;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL ;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SV1;
+UPDATE animals SET weight_kg = (weight_kg * (-1));
+ROLLBACK TO SV1;
+UPDATE animals SET weight_kg = (weight_kg * (-1)) WHERE weight_kg < 0;
+COMMIT;
